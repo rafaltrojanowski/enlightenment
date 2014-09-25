ad5a241f79d48c91c29415ff176a4d71f9104332
Kamil Nogala <kamil.nogala@gmail.com>
2014-09-25 16:35:56 +0200

    dokku v1

--- config/initializers/email_settings.rb
+++ config/initializers/email_settings.rb
@@ -7,11 +7,7 @@ begin
     ActionMailer::Base.smtp_settings = { address: 'localhost', port: 1025 }
 
   elsif Rails.env.production?
-    ActionMailer::Base.default_url_options[:host] = 'base.dev'
-
-    # MailCatcher
-    ActionMailer::Base.delivery_method = :smtp
-    ActionMailer::Base.smtp_settings = { address: 'localhost', port: 1025 }
+    raise StandardError, "You need to set up ActionMailer::Base.default_url_options[:host] and other ActionMailer settings for production environment in #{__FILE__}"
 
   elsif Rails.env.staging?
     raise StandardError, "You need to set up ActionMailer::Base.default_url_options[:host] and other ActionMailer settings for staging environment in #{__FILE__}"
