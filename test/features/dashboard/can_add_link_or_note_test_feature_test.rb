d568d0fadaffd75ef15c05bb9889c37eec740ad7
Rafał Trojanowski <rafal@prograils.com>
2014-09-29 15:10:19 +0200

    dashboard feature test green

--- test/features/dashboard/can_add_link_or_note_test_feature_test.rb
+++ test/features/dashboard/can_add_link_or_note_test_feature_test.rb
@@ -1,14 +1,19 @@
 require 'test_helper'
 
 feature 'Users::Can Add Link Or Note Feature Test' do
+  before do
+    user = FactoryGirl.create(:user)
+
+    login_as(user)
+  end
 
   scenario 'user add note', js: true do
     visit '/dashboard'
 
-    page.text.must_include 'Conent Entities List'
+    page.text.must_include 'List'
 
     fill_in 'content_entity[content]', with: 'my first note'
-    click_button 'Save'
+    click_button 'Add'
 
     page.text.must_include 'my first note'
   end
@@ -16,10 +21,10 @@ feature 'Users::Can Add Link Or Note Feature Test' do
   scenario 'user add link', js: true do
     visit '/dashboard'
 
-    page.text.must_include 'Conent Entities List'
+    page.text.must_include 'List'
 
     fill_in 'content_entity[content]', with: 'http://emberjs.com/guides/routing/defining-your-routes/'
-    click_button 'Save'
+    click_button 'Add'
 
     page.text.must_include 'http://emberjs.com/guides/routing/defining-your-routes/'
   end
