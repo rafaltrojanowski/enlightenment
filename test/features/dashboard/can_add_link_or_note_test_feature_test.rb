1e031100eb26c3df2c6348f20854b305dd05c80d
Rafał Trojanowski <rafal@prograils.com>
2014-09-30 11:48:41 +0200

    authenticate user

--- test/features/dashboard/can_add_link_or_note_test_feature_test.rb
+++ test/features/dashboard/can_add_link_or_note_test_feature_test.rb
@@ -2,12 +2,25 @@ require 'test_helper'
 
 feature 'Users::Can Add Link Or Note Feature Test' do
   before do
-    user = FactoryGirl.create(:user)
+    @user = FactoryGirl.create(:user)
 
-    login_as(user)
+  end
+
+  scenario 'must be logged in to add content', js: true do
+    visit '/dashboard'
+
+    page.text.must_include 'Welcome guest!'
+
+    page.text.wont_include 'List'
+    page.text.wont_include 'Add'
+    page.text.wont_include 'Add content'
+
+    # page.body.wont_include 'content_entity[content]' # TODO
   end
 
   scenario 'user add note', js: true do
+    login_as(@user)
+
     visit '/dashboard'
 
     page.text.must_include 'List'
@@ -19,6 +32,7 @@ feature 'Users::Can Add Link Or Note Feature Test' do
   end
 
   scenario 'user add link', js: true do
+    login_as(@user)
     visit '/dashboard'
 
     page.text.must_include 'List'
