0f5e1ca299ee5b3cbe9cad3b45ae3524b3812d60
Kamil Nogala <kamil.nogala@gmail.com>
2014-10-10 16:18:24 +0200

    forms

--- app/assets/javascripts/holo/scripts.js
+++ app/assets/javascripts/holo/scripts.js
@@ -10,7 +10,17 @@ $('.btn-toggle').click(function() {
 });
 
 // textarea elastic button
-$('#container').on( 'keyup', 'textarea', function (e){
+// $('#container').on( 'keyup', 'textarea', function(e){
+//   $(this).css('height', 'auto' );
+//   $(this).height( this.scrollHeight );
+//   if (this.scrollHeight <= 300) {
+//     $('#add_button').css('height', this.scrollHeight)
+//   }
+// });
+// $('#container').find( 'textarea' ).keyup();
+
+$(document).on("keyup","textarea",function(e) {
+  e.preventDefault();
   $(this).css('height', 'auto' );
   $(this).height( this.scrollHeight );
   if (this.scrollHeight <= 300) {
@@ -20,26 +30,14 @@ $('#container').on( 'keyup', 'textarea', function (e){
 $('#container').find( 'textarea' ).keyup();
 
 // edit button for links/notes
-$(document).on("mouseenter","#link_note a",function(e) {
+$(document).on("mouseenter","#link_note",function(e) {
   e.preventDefault();
   $(this).find("#show_date").hide();
   $(this).find("#edit_button").show();
-}).on("mouseleave","#link_note a",function(e) {
+  $(this).find("#destroy_button").show();
+}).on("mouseleave","#link_note",function(e) {
   e.preventDefault();
   $(this).find("#edit_button").hide();
+  $(this).find("#destroy_button").hide();
   $(this).find("#show_date").show();
 });
-
-
-// function browserWidth(){
-//   var width = $(window).width();
-
-//   if (width <= 752) { // 767?
-//     console.log(width);
-//     $("#dropdown_not_auth").appendTo(".destination");
-//     $("#dropdown_auth").appendTo(".destination");
-//     console.log("dupa");
-//   } 
-// };
-// $(document).ready(browserWidth);
-// $(window).resize(browserWidth);
