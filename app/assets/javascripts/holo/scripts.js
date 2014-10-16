f6849ef9412f809074545cc274ad5420e1376be7
Kamil Nogala <kamil.nogala@gmail.com>
2014-10-16 18:22:45 +0200

    styling comments

--- app/assets/javascripts/holo/scripts.js
+++ app/assets/javascripts/holo/scripts.js
@@ -14,18 +14,18 @@ $(document).on("mouseenter","#link_note",function(e) {
   e.preventDefault();
   $(this).find("#edit_button").show();
   $(this).find("#destroy_button").show();
-  $(this).find("#show_date").mouseenter(function() {
-    $(this).tooltip('show');
-  }).on("mouseleave", "#link_note", function(e) {
-    $(this).tooltip('hide');
-  });
+  // $(this).find("#show_date").mouseenter(function() {
+  //   $(this).tooltip('show');
+  // }).on("mouseleave", "#link_note", function(e) {
+  //   $(this).tooltip('hide');
+  // });
 }).on("mouseleave","#link_note",function(e) {
   e.preventDefault();
   $(this).find("#edit_button").hide();
   $(this).find("#destroy_button").hide();
 });
 
-// $(document).on("mouseenter","#show_date",function(e) {
-//   e.preventDefault();
-//   $(this).tooltip('show');
-// });
+$(document).on("mouseenter","#show_date",function(e) {
+  e.preventDefault();
+  $(this).tooltip('show');
+});
