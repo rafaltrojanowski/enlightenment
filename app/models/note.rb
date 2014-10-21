d6b632045c5e172aac8e7bb34cb3037bb33ef266
Kamil Nogala <kamil.nogala@gmail.com>
2014-10-21 17:13:16 +0200

    fix for showing date and note edit

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,6 @@
 class Note < ActiveRecord::Base
   include Inbox
+  before_create :set_title
 
   TITLE_LENGTH = 20
 
@@ -8,5 +9,10 @@ class Note < ActiveRecord::Base
 
   def to_s
     body
+  end
+
+  def set_title
+    ignore_html = body.gsub( %r{</?[^>]+?>}, '' )
+    self.title = ignore_html[0..TITLE_LENGTH].concat('...')
   end
 end
