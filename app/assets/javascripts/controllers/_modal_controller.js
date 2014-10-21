31e3aaf5b4310f744c3e5f2c3289514968dcce79
Rafał Trojanowski <rafal@prograils.com>
2014-10-21 20:15:53 +0200

    proper way of setting association

--- app/assets/javascripts/controllers/_modal_controller.js
+++ app/assets/javascripts/controllers/_modal_controller.js
@@ -14,24 +14,14 @@ EnlightenmentApp.ModalController = Em.ObjectController.extend({
   actions: {
     save: function() {
       var model = this.get('model');
-      // var group = EnlightenmentApp.Group.find(model.get('group_id'));
-      // group.get('content_entities').pushObject(EnlightenmentApp.ContentEntity.find(model.get('id')));
-      // EnlightenmentApp.store.commit();
-      
-      // var group = this.store.getById('group', model.get('group_id'));
-      // var ce = this.store.getById('content_entity', model.get('id'));
-      // this.get('group').pushObject(group);
-      // console.log(model.get('description'));
-      // var link = this.store.getById('content_entity', model.get('id'));
-      // console.log(this);
-      // console.log(model);
-      // this.get('group').pushObject(group);
-      // group.get('content_entities').pushObject(ce);
+      groupId = model.get('group_id');
+
+      this.store.find('group', groupId).then(function(group) {
+        model.set('group', group);
+      });
+
       this.get('model').save().then(function(){
         alertify.success("Record updated!");
-        // console.log(model.get('group_id'));
-        // model.set('group_id', model.get('group_id'));
-        // console.log(model.get('group_id'));
       },function(response){
       });
     },
