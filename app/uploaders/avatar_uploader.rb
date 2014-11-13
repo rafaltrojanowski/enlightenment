3d338b739a04766aee52776e5ecf156a1715df86
Michal Olejniczak <blind.x6@gmail.com>
2014-11-13 18:06:37 +0100

    default blank-avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -10,6 +10,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
+  def default_url
+    ActionController::Base.helpers.asset_path('blank-avatar.png')
+  end
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
