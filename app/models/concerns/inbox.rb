841f28c4dc3139710652d2dad9fed2d6080e36c5
Rafał Trojanowski <rafal@prograils.com>
2014-11-17 18:01:43 +0100

    wrong delegation fix

--- app/models/concerns/inbox.rb
+++ app/models/concerns/inbox.rb
@@ -4,11 +4,15 @@ module Inbox
     after_commit :leave_inbox_if_need!
   end
 
-  delegate :inbox, :inbox?, :body, to: :content_entity
-
-  private
-
   def leave_inbox_if_need!
     content_entity.update_column(:inbox, "#{!content_entity.group.present?}")
+  end
+
+  def inbox
+    content_entity.inbox
+  end
+
+  def inbox?
+    content_entity.inbox?
   end
 end
