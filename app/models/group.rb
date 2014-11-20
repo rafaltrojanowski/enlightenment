b0ff1bea6afc34e444cd52930667b600b534975f
Rafał Trojanowski <rafal@prograils.com>
2014-11-20 12:14:37 +0100

    group - unecessary validation, dont clear icon on members update, update serializer

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,7 +7,7 @@ class Group < ActiveRecord::Base
 
   validates :name, presence: true
   validates :name, uniqueness: true
-  validates :icon, presence: true, on: :create
+  validates :icon, presence: true
 
   after_destroy do
     content_entities.update_all(inbox: true, group_id: nil)
--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -4,11 +4,16 @@ class GroupSerializer < ActiveModel::Serializer
              :owner_id,
              :icon,
              :users,
-             :user_ids
+             :user_ids,
+             :can_update
+
+  has_many :content_entities
 
   def users
     object.user_ids
   end
 
-  has_many :content_entities
+  def can_update
+    object.owner_id == scope.current_user.try(:id)
+  end
 end
